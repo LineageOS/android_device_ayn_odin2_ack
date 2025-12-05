f006f6c50d19df6a573dc35ae2d0f883d32fa1e8
Aaron Kling <webgeek1234@gmail.com>
2025-12-04 18:17:05 -0600

    Add audio topology binary
    Change-Id: I1ece7d6c039cdff614e5b922426bca15499c92c2

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 
 AB_OTA_UPDATER := true
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/ayn/odin2_ack
+
 include device/ayn/qcs8550-ack/qcs8550.mk
 
 PRODUCT_CHARACTERISTICS   := tv
@@ -18,6 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
+# Audio
+PRODUCT_PACKAGES += \
+    qcom-sm8550-odin2-tplg
 
 # Updater
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
