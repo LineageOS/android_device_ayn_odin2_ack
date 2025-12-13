f051e28879331d5801397c6ee56f1e24c2b7400a
Aaron Kling <webgeek1234@gmail.com>
2025-12-12 23:35:08 -0600

    Add rule to build dtbo images for postinstall processing
    Change-Id: If64573140c252b945e6debb3ba9d0ba6da9e7343

---
+++ build/tasks/dtbo.mk
@@ -0,0 +1,34 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifneq ($(filter odin2_ack odin2_ack_tab, $(TARGET_DEVICE)),)
+MKDTBOIMG := $(HOST_OUT_EXECUTABLES)/mkdtboimg$(HOST_EXECUTABLE_SUFFIX)
+BUILT_TARGET_FILES_ZIPROOT := $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files
+
+# 1: Device variant
+define postinstall_dtbo_rule
+$(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img: $(PRODUCT_OUT)/kernel $(MKDTBOIMG) $(AVBTOOL) $(productimage_intermediates)/file_list.txt
+	$(MKDTBOIMG) create $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img --page_size=$(BOARD_KERNEL_PAGESIZE) $(abspath $(KERNEL_OUT))/qcs8550-ayntec-${1}.dtbo
+	$(AVBTOOL) add_hash_footer --image $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img $(call get-partition-size-argument,$(BOARD_${1}_DTBOIMG_PARTITION_SIZE)) --partition_name dtbo
+	$(hide) grep etc/firmware/${1}_dtbo.img $(productimage_intermediates)/file_list.txt > /dev/null 2>&1 || echo firmware/${1}_dtbo.img >> $(productimage_intermediates)/file_list.txt
+
+$(TARGET_OUT_PRODUCT_EXECUTABLES)/ayn_bootloader_payload_updater: $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img
+
+.PHONY: $(1)_dtboimage
+$(1)_dtboimage: $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img
+
+$(BUILT_TARGET_FILES_ZIPROOT).zip: $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/${1}_dtbo.img
+
+$(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/${1}_dtbo.img: $(BUILT_TARGET_FILES_ZIPROOT).zip.list $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img
+	@mkdir -p $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES
+	@cp $(TARGET_OUT_PRODUCT_ETC)/firmware/${1}_dtbo.img $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/${1}_dtbo.img
+	@grep $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/${1}_dtbo.img $(BUILT_TARGET_FILES_ZIPROOT).zip.list > /dev/null 2>&1 || echo $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/${1}_dtbo.img >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
+endef
+
+$(eval $(call postinstall_dtbo_rule,odin2))
+$(eval $(call postinstall_dtbo_rule,odin2mini))
+$(eval $(call postinstall_dtbo_rule,odin2portal))
+$(eval $(call postinstall_dtbo_rule,thor))
+endif
