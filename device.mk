9979776d55d8cba15d7a0db8b339b50cc9823acf
Aaron Kling <webgeek1234@gmail.com>
2026-01-01 00:44:36 -0600

    Support baylibre audio hal
    * Add control initialization
    * Wait during early boot for sound driver to finish init
    * Set card name to prevent probe order issues with usb cards
    
    Change-Id: I7e60ee4b2b9f05157ca4876e52ac8562a0fb6517

--- device.mk
+++ device.mk
@@ -42,6 +42,13 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     qcom-sm8550-odin2-topology
 
+ifeq ($(TARGET_AUDIO_HAL),baylibre)
+PRODUCT_COPY_FILES += \
+    device/ayn/odin2_ack/audio/mixer_controls.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_controls.xml
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.primary.card_name=AYNOdin2
+endif
+
 # Firmware
 $(call soong_config_set_bool,linux_firmware_mainline,use_product_specific_ath_board2,true)
 PRODUCT_PACKAGES += \
